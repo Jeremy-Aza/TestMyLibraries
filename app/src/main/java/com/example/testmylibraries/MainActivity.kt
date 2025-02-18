ecdfcde1817905a6e6b79c63d6fa241c83055e57
Jeremy Aza <jeremy.aza@sumamexico.com>
2025-02-17 20:37:06 -0500

    feat: testing project for integration and functional testing of libraries

---
+++ app/src/main/java/com/example/testmylibraries/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.testmylibraries
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import com.example.vdid.ContentLibrary
+import com.example.vdid_document.MyLibrary
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myFragment = MyLibrary.newInstance()
+
+        supportFragmentManager.commit {
+            replace(
+                R.id.fragmentContainer,
+                myFragment
+            )
+        }
+
+    }
+}
+
+
---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+import java.net.URI
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        flatDir {
+            dirs("libs")
+        }
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = URI("https://maven.innovatrics.com/releases") }
+    }
+}
+
+rootProject.name = "TestMyLibraries"
+include(":app")
+ 
