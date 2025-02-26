e9ff10089f23e23aa3af06178dead311355d78a1
Jeremy Aza <jeremy.aza@sumamexico.com>
2025-02-26 00:59:12 -0500

    feat: changes in the integration, desgin and management of function for the dcument capture process

--- app/src/main/java/com/example/testmylibraries/MainActivity.kt
+++ app/src/main/java/com/example/testmylibraries/MainActivity.kt
@@ -1,19 +1,21 @@
 package com.example.testmylibraries
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import com.example.vdid.ContentLibrary
+import com.example.vdid.OnProcessListener
 
 //import com.example.vdid.ContentLibrary
 //import com.example.vdid_document.MyLibrary
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnProcessListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val myFragment = ContentLibrary.newFragment()
+        val myFragment = ContentLibrary.autocaptureFragment(this)
 
         supportFragmentManager.commit {
             replace(
@@ -22,7 +24,24 @@ class MainActivity : AppCompatActivity() {
             )
         }
 
+
+        val button: Button = this.findViewById(R.id.btnShowMessage)
+        button.setOnClickListener {
+            ContentLibrary.continueDetection()
+            Toast.makeText(this, "Proceso iniciado", Toast.LENGTH_SHORT).show()
+        }
     }
+
+
+    override fun onCaptureFailed(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDocumentCaptured(image: String) {
+        println("####################Get image $image")
+    }
+
+
 }
 
 
